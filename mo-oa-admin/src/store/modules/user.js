66b2535db6010ace6a3d162221df82bc25a200fa
mozihao_glb <mozihao_glb@qq.com>
2023-10-09 10:51:46 +0800

    前端权限对接整合

--- mo-oa-admin/src/store/modules/user.js
+++ mo-oa-admin/src/store/modules/user.js
@@ -6,7 +6,9 @@ const getDefaultState = () => {
   return {
     token: getToken(),
     name: '',
-    avatar: ''
+    avatar: '',
+    buttons: [],//新增
+    menus: ''//新增
   }
 }
 
@@ -24,6 +26,14 @@ const mutations = {
   },
   SET_AVATAR: (state, avatar) => {
     state.avatar = avatar
+  },
+  // 新增
+  SET_BUTTONS: (state, buttons) => {
+    state.buttons = buttons
+  },
+  // 新增
+  SET_MENUS: (state, menus) => {
+    state.menus = menus
   }
 }
 
@@ -46,7 +56,7 @@ const actions = {
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
+      getInfo().then(response => {
         const { data } = response
 
         if (!data) {
@@ -57,6 +67,9 @@ const actions = {
 
         commit('SET_NAME', name)
         commit('SET_AVATAR', avatar)
+
+        commit("SET_BUTTONS", data.buttons)
+        commit("SET_MENUS", data.routers)
         resolve(data)
       }).catch(error => {
         reject(error)
